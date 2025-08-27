e3d8728c7a4ea45d684ee62d83897cb0f0df8b83
Burcu Gunes <burcudagli.10@gmail.com>
2025-08-27 11:00:29 +0100

    updated moodTracker component

--- src/components/MoodTracker.jsx
+++ src/components/MoodTracker.jsx
@@ -1,5 +1,7 @@
-import { IconButton, Box, Button, Icon } from "@mui/material";
+import { IconButton, Box, Button } from "@mui/material";
 import { useState } from "react";
+import saveMoodHistory from "../functions/SaveMood";
+
 import {
   SentimentVeryDissatisfied,
   SentimentDissatisfied,
@@ -8,11 +10,25 @@ import {
   SentimentVerySatisfied,
 } from "@mui/icons-material";
 
-const moodColors = ["#FFADAD", "#FFD6A5", "#FDFFB6", "#CAFFBF", "#9BF6FF"];
+const moodColors = [
+  "bg-[#FFADAD]",
+  "bg-[#FFD6A5]",
+  "bg-[#FDFFB6]",
+  "bg-[#CAFFBF]",
+  "bg-[#9BF6FF]",
+];
 const moodNames = ["Very Sad", "Sad", "Neutral", "Happy", "Very Happy"];
 
 function MoodTracker() {
   const [selected, setSelected] = useState(null);
+
+  const handleSaveMood = () => {
+    if (selected !== null) {
+      saveMoodHistory(selected + 1);
+      setSelected(null);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center space-y-4">
       <Box className="flex justify-between max-w-md w-full">
@@ -22,16 +38,28 @@ function MoodTracker() {
           SentimentNeutral,
           SentimentSatisfied,
           SentimentVerySatisfied,
-        ].map((Icon, idx) => (
-          <div key={idx} className="flex flex-col items-center">
-            <IconButton onClick={() => setSelected(idx)}>
-              <Icon></Icon>
+        ].map((IconComponent, index) => (
+          <div key={index} className="flex flex-col items-center">
+            <IconButton onClick={() => setSelected(index)}>
+              <IconComponent
+                className={`rounded-full transition duration-300 mx-2 shadow-md 
+  ${selected === index ? moodColors[index] + " shadow-lg" : "bg-gray-200"}
+`}
+              />
             </IconButton>
-            <span className="text-sm text-gray-700 mt-1">{moodNames[idx]}</span>
+
+            <span className="text-sm text-blue-950 mt-1">
+              {moodNames[index]}
+            </span>
           </div>
         ))}
       </Box>
-      <Button variant="contained" color="primary">
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleSaveMood}
+        disabled={selected === null}
+      >
         Save
       </Button>
     </div>
